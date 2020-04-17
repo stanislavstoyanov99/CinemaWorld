28c2b92bf1937e7680617ec676535a5373b25ad1
Stanislav Stoyanov <slavkata_99@abv.bg>
2020-04-17 13:05:01 +0300

    Implement movie projection page for user to buy a ticket. Add jquery nice scroll.

---
+++ src/Web/CinemaWorld.Web/Controllers/TicketsController.cs
@@ -0,0 +1,30 @@
+﻿namespace CinemaWorld.Web.Controllers
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+
+    using CinemaWorld.Models.ViewModels.MovieProjections;
+    using CinemaWorld.Services.Data.Contracts;
+
+    using Microsoft.AspNetCore.Mvc;
+
+    public class TicketsController : Controller
+    {
+        private readonly IMovieProjectionsService movieProjectionsService;
+
+        public TicketsController(IMovieProjectionsService movieProjectionsService)
+        {
+            this.movieProjectionsService = movieProjectionsService;
+        }
+
+        public async Task<IActionResult> Book(int id)
+        {
+            var movieProjection = await this.movieProjectionsService
+                .GetViewModelByIdAsync<MovieProjectionDetailsViewModel>(id);
+
+            return this.View(movieProjection);
+        }
+    }
+}
