f1471c0075ed45b3a07e3d37fbcfd687580d2bca
Stanislav Stoyanov <slavkata_99@abv.bg>
2020-09-27 23:05:54 +0300

    Finish Halls Service tests.

--- src/Tests/CinemaWorld.Services.Data.Tests/HallsServiceTests.cs
+++ src/Tests/CinemaWorld.Services.Data.Tests/HallsServiceTests.cs
@@ -1,6 +1,7 @@
 ﻿namespace CinemaWorld.Services.Data.Tests
 {
     using System;
+    using System.Linq;
     using System.Reflection;
     using System.Threading.Tasks;
 
@@ -9,6 +10,7 @@
     using CinemaWorld.Data.Models.Enumerations;
     using CinemaWorld.Data.Repositories;
     using CinemaWorld.Models.InputModels.AdministratorInputModels.Halls;
+    using CinemaWorld.Models.ViewModels.Halls;
     using CinemaWorld.Services.Data.Common;
     using CinemaWorld.Services.Data.Contracts;
     using CinemaWorld.Services.Mapping;
@@ -16,6 +18,7 @@
     using Microsoft.Data.Sqlite;
     using Microsoft.EntityFrameworkCore;
 
+    using Newtonsoft.Json;
     using Xunit;
 
     public class HallsServiceTests : IDisposable
@@ -84,6 +87,22 @@
         }
 
         [Fact]
+        public async Task CheckAddingHallWithInvalidCategoryType()
+        {
+            this.SeedDatabase();
+
+            var hall = new HallCreateInputModel
+            {
+                Category = HallCategory.Small.ToString() + "Invalid type",
+                Capacity = 20,
+            };
+
+            var exception = await Assert
+                .ThrowsAsync<ArgumentException>(async () => await this.hallsService.CreateAsync(hall));
+            Assert.Equal(string.Format(ExceptionMessages.InvalidHallCategoryType, hall.Category), exception.Message);
+        }
+
+        [Fact]
         public async Task CheckIfAddingHallReturnsViewModel()
         {
             var hall = new HallCreateInputModel
@@ -98,6 +117,120 @@
             Assert.Equal(dbEntry.Id, viewModel.Id);
             Assert.Equal(dbEntry.Category, viewModel.Category);
             Assert.Equal(dbEntry.Capacity, viewModel.Capacity);
+        }
+
+        [Fact]
+        public async Task CheckIfDeletingHallWorksCorrectly()
+        {
+            this.SeedDatabase();
+
+            await this.hallsService.DeleteByIdAsync(this.firstHall.Id);
+
+            var count = await this.hallsRepository.All().CountAsync();
+
+            Assert.Equal(0, count);
+        }
+
+        [Fact]
+        public async Task CheckIfDeletingHallReturnsNullReferenceException()
+        {
+            this.SeedDatabase();
+
+            var exception = await Assert
+                .ThrowsAsync<NullReferenceException>(async () => await this.hallsService.DeleteByIdAsync(3));
+            Assert.Equal(string.Format(ExceptionMessages.HallNotFound, 3), exception.Message);
+        }
+
+        [Fact]
+        public async Task CheckIfEditingHallWorksCorrectly()
+        {
+            this.SeedDatabase();
+
+            var hallEditViewModel = new HallEditViewModel
+            {
+                Id = this.firstHall.Id,
+                Category = HallCategory.Medium.ToString(),
+                Capacity = 35,
+            };
+
+            await this.hallsService.EditAsync(hallEditViewModel);
+
+            Assert.Equal(hallEditViewModel.Category, this.firstHall.Category.ToString());
+            Assert.Equal(hallEditViewModel.Capacity, this.firstHall.Capacity);
+        }
+
+        [Fact]
+        public async Task CheckIfEditingHallReturnsNullReferenceException()
+        {
+            this.SeedDatabase();
+
+            var hallEditViewModel = new HallEditViewModel
+            {
+                Id = 3,
+                Category = HallCategory.Small.ToString(),
+            };
+
+            var exception = await Assert
+                .ThrowsAsync<NullReferenceException>(async () => await this.hallsService.EditAsync(hallEditViewModel));
+            Assert.Equal(string.Format(ExceptionMessages.HallNotFound, hallEditViewModel.Id), exception.Message);
+        }
+
+        [Fact]
+        public async Task CheckEditingHallWithInvalidCategoryType()
+        {
+            this.SeedDatabase();
+
+            var hallEditViewModel = new HallEditViewModel
+            {
+                Id = this.firstHall.Id,
+                Category = "Invalid category",
+                Capacity = 10,
+            };
+
+            var exception = await Assert
+                .ThrowsAsync<ArgumentException>(async () => await this.hallsService.EditAsync(hallEditViewModel));
+            Assert.Equal(string.Format(ExceptionMessages.InvalidHallCategoryType, hallEditViewModel.Category), exception.Message);
+        }
+
+        [Fact]
+        public async Task CheckIfGetAllHallsAsyncWorksCorrectly()
+        {
+            this.SeedDatabase();
+
+            var result = await this.hallsService.GetAllHallsAsync<HallDetailsViewModel>();
+
+            var count = result.Count();
+            Assert.Equal(1, count);
+        }
+
+        [Fact]
+        public async Task CheckIfGetHallViewModelByIdAsyncWorksCorrectly()
+        {
+            this.SeedDatabase();
+
+            var expectedModel = new HallDetailsViewModel
+            {
+                Id = this.firstHall.Id,
+                Category = this.firstHall.Category,
+                Capacity = this.firstHall.Capacity,
+            };
+
+            var viewModel = await this.hallsService.GetViewModelByIdAsync<HallDetailsViewModel>(this.firstHall.Id);
+
+            var expectedObj = JsonConvert.SerializeObject(expectedModel);
+            var actualResultObj = JsonConvert.SerializeObject(viewModel);
+
+            Assert.Equal(expectedObj, actualResultObj);
+        }
+
+        [Fact]
+        public async Task CheckIfGetViewModelByIdAsyncThrowsNullReferenceException()
+        {
+            this.SeedDatabase();
+
+            var exception = await Assert
+                .ThrowsAsync<NullReferenceException>(async () => await this.hallsService.GetViewModelByIdAsync<HallDetailsViewModel>(3));
+            Assert.Equal(string.Format(ExceptionMessages.HallNotFound, 3), exception.Message);
         }
 
         public void Dispose()
