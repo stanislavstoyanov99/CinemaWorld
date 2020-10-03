7d561ad04046a2d8c560701b2ce7fc6332f09f18
Stanislav Stoyanov <slavkata_99@abv.bg>
2020-10-03 22:40:02 +0300

    Initialize Ratings Service tests.

---
+++ src/Tests/CinemaWorld.Services.Data.Tests/RatingsServiceTests.cs
@@ -0,0 +1,121 @@
+﻿namespace CinemaWorld.Services.Data.Tests
+{
+    using System;
+    using System.Reflection;
+    using System.Threading.Tasks;
+
+    using CinemaWorld.Data;
+    using CinemaWorld.Data.Models;
+    using CinemaWorld.Data.Models.Enumerations;
+    using CinemaWorld.Data.Repositories;
+    using CinemaWorld.Services.Data.Contracts;
+    using CinemaWorld.Services.Mapping;
+
+    using Microsoft.Data.Sqlite;
+    using Microsoft.EntityFrameworkCore;
+
+    public class RatingsServiceTests : IDisposable
+    {
+        private readonly IRatingsService ratingsService;
+        private EfDeletableEntityRepository<StarRating> starRatingsRepository;
+        private EfDeletableEntityRepository<Movie> moviesRepository;
+        private EfDeletableEntityRepository<Director> directorsRepository;
+        private SqliteConnection connection;
+
+        private StarRating firstStarRating;
+        private Director firstDirector;
+        private Movie firstMovie;
+
+        public RatingsServiceTests()
+        {
+            this.InitializeMapper();
+            this.InitializeDatabaseAndRepositories();
+            this.InitializeFields();
+
+            this.ratingsService = new RatingsService(this.starRatingsRepository);
+        }
+
+        public void Dispose()
+        {
+            this.connection.Close();
+            this.connection.Dispose();
+        }
+
+        private void InitializeDatabaseAndRepositories()
+        {
+            this.connection = new SqliteConnection("DataSource=:memory:");
+            this.connection.Open();
+            var options = new DbContextOptionsBuilder<CinemaWorldDbContext>().UseSqlite(this.connection);
+            var dbContext = new CinemaWorldDbContext(options.Options);
+
+            dbContext.Database.EnsureCreated();
+
+            this.starRatingsRepository = new EfDeletableEntityRepository<StarRating>(dbContext);
+            this.moviesRepository = new EfDeletableEntityRepository<Movie>(dbContext);
+            this.directorsRepository = new EfDeletableEntityRepository<Director>(dbContext);
+        }
+
+        private void InitializeFields()
+        {
+            this.firstDirector = new Director
+            {
+                FirstName = "Peter",
+                LastName = "Kirilov",
+            };
+
+            this.firstMovie = new Movie
+            {
+                Name = "Titanic",
+                DateOfRelease = DateTime.UtcNow,
+                Resolution = "HD",
+                Rating = 7.80m,
+                Description = "Test description here",
+                Language = "English",
+                CinemaCategory = CinemaCategory.B,
+                TrailerPath = "test trailer path",
+                CoverPath = "test cover path",
+                WallpaperPath = "test wallpaper path",
+                IMDBLink = "test imdb link",
+                Length = 120,
+                DirectorId = 1,
+            };
+
+            this.firstStarRating = new StarRating
+            {
+                Rate = 5,
+                MovieId = 1,
+                NextVoteDate = DateTime.UtcNow.AddDays(1),
+            };
+        }
+
+        private async void SeedDatabase()
+        {
+            await this.SeedDirectors();
+            await this.SeedMovies();
+        }
+
+        private async Task SeedStarRatings()
+        {
+            await this.starRatingsRepository.AddAsync(this.firstStarRating);
+
+            await this.starRatingsRepository.SaveChangesAsync();
+        }
+
+        private async Task SeedMovies()
+        {
+            await this.moviesRepository.AddAsync(this.firstMovie);
+
+            await this.moviesRepository.SaveChangesAsync();
+        }
+
+        private async Task SeedDirectors()
+        {
+            await this.directorsRepository.AddAsync(this.firstDirector);
+
+            await this.directorsRepository.SaveChangesAsync();
+        }
+
+        private void InitializeMapper() => AutoMapperConfig.
+            RegisterMappings(Assembly.Load("CinemaWorld.Models.ViewModels"));
+    }
+}
