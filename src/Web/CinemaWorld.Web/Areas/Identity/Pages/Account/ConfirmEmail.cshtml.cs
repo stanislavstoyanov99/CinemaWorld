9f7afbd0ac3662e14cd5f5edc0a7a2cfc4343877
Stanislav Stoyanov <slavkata_99@abv.bg>
2021-05-05 10:03:42 +0300

    Update travis.yml

---
+++ src/Web/CinemaWorld.Web/Areas/Identity/Pages/Account/ConfirmEmail.cshtml.cs
@@ -0,0 +1,46 @@
+﻿namespace CinemaWorld.Web.Areas.Identity.Pages.Account
+{
+    using System.Text;
+    using System.Threading.Tasks;
+
+    using CinemaWorld.Data.Models;
+
+    using Microsoft.AspNetCore.Authorization;
+    using Microsoft.AspNetCore.Identity;
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.AspNetCore.Mvc.RazorPages;
+    using Microsoft.AspNetCore.WebUtilities;
+
+    [AllowAnonymous]
+    public class ConfirmEmailModel : PageModel
+    {
+        private readonly UserManager<CinemaWorldUser> userManager;
+
+        public ConfirmEmailModel(UserManager<CinemaWorldUser> userManager)
+        {
+            this.userManager = userManager;
+        }
+
+        [TempData]
+        public string StatusMessage { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(string userId, string code)
+        {
+            if (userId == null || code == null)
+            {
+                return this.RedirectToPage("/Index");
+            }
+
+            var user = await this.userManager.FindByIdAsync(userId);
+            if (user == null)
+            {
+                return this.NotFound($"Unable to load user with ID '{userId}'.");
+            }
+
+            code = Encoding.UTF8.GetString(WebEncoders.Base64UrlDecode(code));
+            var result = await this.userManager.ConfirmEmailAsync(user, code);
+            this.StatusMessage = result.Succeeded ? "Thank you for confirming your email." : "Error confirming your email.";
+            return this.Page();
+        }
+    }
+}
---
+++ src/Web/CinemaWorld.Web/Areas/Identity/Pages/Account/ForgotPasswordConfirmation.cshtml.cs
@@ -0,0 +1,17 @@
+﻿namespace CinemaWorld.Web.Areas.Identity.Pages.Account
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+
+    using Microsoft.AspNetCore.Authorization;
+    using Microsoft.AspNetCore.Mvc.RazorPages;
+
+    [AllowAnonymous]
+    public class ForgotPasswordConfirmation : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
